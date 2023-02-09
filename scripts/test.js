ea3637fe8e3f8f0502c3608ada789c26352ce026
Sera <sergolova666@tutanota.com>
2023-02-09 22:00:55 +0200

    Начал делать О нас

---
+++ scripts/test.js
@@ -0,0 +1,7 @@
+// let a=document.getElementById('mySpan');
+// let r=document.getElementById('rect');
+// let v = 0;
+// let t = setInterval(h=>{
+//   a.innerText = v++;
+//   r.style.width = 50+v+'px';
+// }, 00);
