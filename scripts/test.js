3223ed90cc427a26512077de1cb7fabf4aac8541
Sera <sergolova666@tutanota.com>
2023-02-12 22:45:32 +0200

    Об авторе

--- scripts/test.js
+++ scripts/test.js
@@ -1,7 +1,14 @@
-// let a=document.getElementById('mySpan');
-// let r=document.getElementById('rect');
-// let v = 0;
-// let t = setInterval(h=>{
-//   a.innerText = v++;
-//   r.style.width = 50+v+'px';
-// }, 00);
+// let acc=document.getElementsByClassName('accordion');
+// if (acc && acc.length>0) {
+//    Array.prototype.forEach.call(acc, a=>{
+//      a.addEventListener('focusout', e=>{
+//        a.style.width = '50px'
+//      })
+//    });
+// }
+// // let r=document.getElementById('rect');
+// // let v = 0;
+// // let t = setInterval(h=>{
+// //   a.innerText = v++;
+// //   r.style.width = 50+v+'px';
+// // }, 00);
